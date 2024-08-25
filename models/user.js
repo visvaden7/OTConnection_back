84bed6762733eb7e73dd3598e40f46169d0ebf1e
visvaden7 <visvaden7@naver.com>
2024-08-26 08:06:03 +0900

    feat: 카카오 로그인 기능 구현
    세션유지 (백엔드에서 쿠키관리 기능)
    API - 유저 (이메일, 닉네임, 프로필사진) 제공

--- models/user.js
+++ models/user.js
@@ -11,7 +11,6 @@ class User extends Sequelize.Model {
                 nick: {
                     type: Sequelize.STRING(15),
                     allowNull: false,
-                    unique: true,
                 },
                 password: {
                     type: Sequelize.STRING(100),
