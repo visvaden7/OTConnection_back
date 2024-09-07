e7ebc1d9a3461a7ca86bc99e0710271cf0d6b648
visvaden7 <visvaden7@naver.com>
2024-09-08 03:50:36 +0900

    feat: ipInfo API 추가
    nowBestWebtoon,
    recommandByGenre,
    해당 기능 추가

--- controller/ipInfo.js
+++ controller/ipInfo.js
@@ -1,5 +1,5 @@
 const {Ip} = require("../models/")
-const {Op} = require("sequelize");
+const {Op, Sequelize} = require("sequelize");
 
 exports.focusWebtoonOttComboData = async (req, res) => {
     const sixMonthsAgo = new Date()
@@ -31,4 +31,64 @@ exports.focusWebtoonOttComboData = async (req, res) => {
     console.log(comboData)
     comboData.sort((a,b) => b.total_rating - a.total_rating)
     res.json(comboData.slice(0,3))
+}
+
+exports.nowBestWebtoon = async (req, res) => {
+    try {
+        const sixMonthsAgo = new Date()
+        sixMonthsAgo.setMonth(sixMonthsAgo.getMonth() - 24)
+        const webtoonList = await Ip.findAll({
+            where: {
+                [Op.or]: [
+                    { webtoon_end_date: { [Op.is]: null } }, // webtoon_end_date가 null인 경우
+                    { webtoon_end_date: { [Op.gte]: sixMonthsAgo } } // webtoon_end_date가 6개월 전 이후인 경우
+                ]
+            },
+            order: [
+                ['comments','DESC'],
+                [Sequelize.literal('ISNULL(webtoon_end_date)'), 'DESC'], // NULL이 먼저 오게 정렬
+                ['webtoon_end_date', 'DESC']  // 나머지는 최신 날짜 순으로 정렬
+            ],
+        });
+        //TODO: sort하는 방법 total rating 기준으로
+        const top4 = webtoonList.reduce((top4Array, webtoon,idx) => {
+            const object = {id: webtoon.dataValues.ip_id, title: webtoon.dataValues.title, webtoon:webtoon.dataValues.webtoon_platform, total_rating: (webtoon.dataValues.rating* 0.5 + webtoon.dataValues.imdb_rating*0.5).toFixed(1), view:webtoon.dataValues.total_views ?? 0, poster: webtoon.dataValues.webtoon_profile_link, rank: idx + 1 };
+            top4Array.push(object)
+            return top4Array
+        },[])
+        res.json(top4.slice(0,4))
+    } catch(err) {
+        console.log(err)
+        res.json({error: 'Failed to fetch webtoon list'})
+    }
+}
+
+exports.recommandByGenre = async(req, res) => {
+    try {
+        const genre = decodeURIComponent(req.query.genre);
+        const recommandOttData = await Ip.findAll({
+            where: {
+                ott_platform: {
+                    [Op.ne]: null,
+                    [Op.ne]: ''
+                },
+                webtoon_platform: {
+                    [Op.ne]: null,
+                    [Op.ne]: ''
+                },
+                genre: {
+                    [Op.like]: `%${genre}%`
+                }
+            },
+            order: [
+                ['total_views','DESC']
+            ]
+        })
+        //TODO: OTT작품이미지, OTT제목, 장르, OTT_PLATFORM,
+        //TODO: Webtoon작품이미지, 웹툰제목, 장르,
+        res.json('test중')
+    } catch (err) {
+        console.log(err)
+        res.json({error: 'Failed to fetch recommand list'})
+    }
 }
--- routes/ipInfo.js
+++ routes/ipInfo.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const router = express.Router()
-const {focusWebtoonOttComboData} = require("../controller/ipInfo");
+const {focusWebtoonOttComboData, nowBestWebtoon, recommandByGenre} = require("../controller/ipInfo");
 
 router.get("/focusWebtoonOttComboData", focusWebtoonOttComboData)
+router.get("/nowBestWebtoon", nowBestWebtoon)
+//example url : /recommand?genre="로맨스"
+router.get("/recommand",recommandByGenre)
 
 module.exports = router;
